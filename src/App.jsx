33e93bf8daebabf6c6e174dcc1f1fa0e226c1308
Ajit133 <dasajit697@gmail.com>
2024-02-10 01:43:49 +0600

    work with pixabay api and set the api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,29 @@
 import { useState,useEffect } from 'react'
+import ImageCard from './Components/ImageCard';
 const apiUrl = import.meta.env.VITE_REACT_APP_API_KEY;
 
 function App() {
-  // const [term,setTerm] = useState('');
+  const [images,setImages] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+  const [term,setTerm] = useState('');
 
-  // useEffect(()=>{
-  //   fetch(`https://pixabay.com/api/?key=${apiUrl}&q=${term}&image_type=photo&pretty=true`)
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  //     .catch(err => console.log(err));
-  // },[]);
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${apiUrl}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  },[]);
    
   
   return (
-    <>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-             <img src='https://source.unsplash.com/random' alt='images' className='w-full'/>
-        <div className='px-6 py-4'>
-           <div className='font-bold text-purple-500 text-xl mb-2'>
-               Photo By Ajit Das
-           </div>
-           <ul>
-            <li>
-              <strong>Views : </strong>
-               4000
-            </li>
-            <li>
-              <strong>Downloads : </strong>
-               300
-            </li>
-            <li>
-              <strong>Likes : </strong>
-               400
-            </li>
-           </ul>
-        </div>
-
-        <div className='px-6 py-4'>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                  #tag1
-            </span>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                  #tag2
-            </span>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                  #tag3
-            </span>
-        </div>
-        </div>  
-    </>
+    <div className='container mx-auto'>
+        {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32'>Loading....</h1>  : <div className='grid grid-cols-3 gap-4'>
+             {images.map(image =>(<ImageCard key={image.id} image={image} />))}
+        </div>}
+    </div>
   )
 }
 
